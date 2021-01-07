e921e89831f7fab0a35fa074ed8214ddfd50fabc
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 12:16:33 +0100

    träning om olika Return

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ document.getElementById("test").innerHTML = myFunction;
 }
 */
 
-
+/*
 function myInfo() {
 
     "use strict"
@@ -102,3 +102,91 @@ function myInfo() {
 var myFunction = myInfo();
 
 document.getElementById("test").innerHTML = myFunction;
+*/
+/*
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+        myAge = 32;
+
+    return 100 + 20;
+
+}
+
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;
+*/
+/*
+function myInfo() {
+
+    "use strict"
+
+    return 100 + 20;
+}
+
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;
+*/
+
+/*
+function myInfo() {
+
+    "use strict"
+
+    return "Jss";
+}
+
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;
+*/
+/*
+function myAgeEnDays() {
+
+    "use strict"
+
+    var 
+        myAge = 30;
+
+    return myAge * 365;
+
+}
+
+var myFunction = myAgeEnDays();
+
+document.getElementById("test").innerHTML ="Your Age In Days = " + myFunction;
+*/
+/*
+function myAgeEnDays() {
+
+    "use strict"
+
+    var myAge = 30;
+
+    return myAge * 365;
+
+}
+
+var ageCalc = myAgeEnDays();
+
+document.getElementById("test").innerHTML = "Your Age In Days = " + ageCalc;
+*/
+
+function myAgeEnDays() {
+
+    "use strict"
+
+    var myAge = 30;
+
+    return myAge * 365;
+
+}
+
+var ageCalc = myAgeEnDays();
+
+document.getElementById("test").innerHTML = "Your Age In Days = " + ageCalc + " days";
