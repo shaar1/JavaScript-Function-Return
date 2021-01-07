8d16a82a0bd45a92627c854a7dd33ffb27f32ab1
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 12:07:40 +0100

    träning om olika function

---
+++ main.js
@@ -0,0 +1,104 @@
+/* 
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+    myAge = "32"
+
+    return myName;
+
+}
+
+myInfo();
+
+document.getElementById("test").innerHTML = myInfo();
+*/
+/*
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+    myAge = "32"
+
+    return myAge;
+
+}
+
+myInfo();
+
+document.getElementById("test").innerHTML = myInfo();
+*/
+/*
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+    myAge = "32"
+
+   // return myAge;
+
+}
+
+myInfo();
+
+document.getElementById("test").innerHTML = myInfo();
+*/
+/*
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+        myAge = "32"
+
+    return myAge;
+
+}
+
+var myfunction = myInfo();
+
+document.getElementById("test").innerHTML = myInfo();
+*/
+
+/*
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+        myAge = "32";
+
+    return myAge;
+
+}
+
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;
+}
+*/
+
+
+function myInfo() {
+
+    "use strict"
+
+    var myName = "Abdallah",
+
+        myAge = 32;
+
+    return myAge + 20;
+
+}
+
+var myFunction = myInfo();
+
+document.getElementById("test").innerHTML = myFunction;
